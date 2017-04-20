f18c13ac53c3772ca293f3db388c3bb3bcd47cce
Tim Frankie <tfrankie88@gmail.com>
2017-04-20 11:56:30 -0400

    adding media for social

--- app.js
+++ app.js
@@ -2,21 +2,22 @@
 
 $(document).ready(function(){
 
-// creat array for populated game board
-var $wordZ = [
-  'doctype',
-  'css',
-  'array',
-  'html',
-  'server',
-  'jquery',
-  'github',
-  'object',
-  'method',
-  'flexbox',
-  'class'
-];
-// on load, game board creates li items
+  // creat array for populated game board
+  var $wordZ = [
+    'doctype',
+    'css',
+    'array',
+    'html',
+    'server',
+    'jquery',
+    'github',
+    'object',
+    'method',
+    'flexbox',
+    'class'
+  ];
+
+  // on load, game board creates li items
   var $letterInput = $('input');
   var $addLetter   = $('#submit-letter');
   var $randoNum    = Math.floor(Math.random()*($wordZ.length));
@@ -24,56 +25,68 @@ var $wordZ = [
   var $splitWord   = $wordSelect.split('');
   var $count       = 0;
 
-// As explained by Bobby; after splitting array item word, it is reassigned to a li item and the letter is hidden but the data is kept
+  // As explained by Bobby; after splitting array item word, it is reassigned to
+  // a li item and the letter is hidden but the data is kept
   $splitWord.forEach(function(letter) {
-    var $li = $(`<li class = "not-found" data-letter = ${letter}></li>`);
+    var $li = $(`<li class="not-found" data-letter="${letter}"></li>`);
     $('.word').append($li);
     $('li').css('border-bottom', '2px solid white');
-// console.log(letter);
   });
 
-// Check if Input is equal to any of the data items, if so reveal the data-letter in the corresponding li item
+  // Check if Input is equal to any of the data items, if so reveal the
+  // data-letter in the corresponding li item
 
-// For Keypress
-$('input').on('keypress', function(event) {
-  if (event.which === 13) {
-// Filter through split array item to see if input matches any of the letters
-    var $notFound = true;
-    for (var i = 0; i < $splitWord.length; i++) {
-      var $rightItem = $('.word li').eq(i);
-      if ($letterInput.val().toLowerCase() === $rightItem.attr('data-letter')) {
-// console.log('Add correct letter')
-// If letter matches data in li item attach input value to li
-        $rightItem.text($rightItem.attr('data-letter'));
-        $rightItem.css('border-bottom', '');
-//Set Counter for winner
-        $count += 1;
-// Show every instance of letter...Thanks Bobby
-        $notFound = false;
-      };
-    };
-// If input is incorrect remove white div box which was concealing img underneath
-    if($notFound) {
-      $('.lilGuy div').first().remove();
-// Append wrong letter to div field of wrong letters
-      var letterInputVal = $letterInput.val();
-      var wrongLetterText = $('.wrongLetter').text();
-      $('.wrongLetter').text(letterInputVal + wrongLetterText);
-    };
-// Clear input field
-    $letterInput.val('');
-// Declare winner or loser
-    var winner = function () {
-      if ($count === $('li').length) {
-        alert('Congrats! You win. Refresh page, and play again!');
-      };
-    };
-    loser();
-    winner();
-  };
-});
+  // Keypress event listener
+  $('input').on('keypress', function(event) {
 
-// On click event instead of keypress
+    // Checking for a keypress of the ENTER key
+    if (event.which === 13) {
+
+      // Filter through split array item to see if input matches any of the letters
+      var $notFound = true;
+      for (var i = 0; i < $splitWord.length; i++) {
+        var $rightItem = $('.word li').eq(i);
+        if ($letterInput.val().toLowerCase() === $rightItem.attr('data-letter')) {
+
+          // If letter matches data in li item attach input value to li
+          $rightItem.text($rightItem.attr('data-letter'));
+          $rightItem.css('border-bottom', '');
+
+          //Set Counter for winner
+          $count += 1;
+
+          // Show every instance of letter...Thanks Bobby
+          $notFound = false;
+        };
+      };
+
+      // If input is incorrect remove white div box which was concealing img underneath
+      if($notFound) {
+        $('.lilGuy div').first().remove();
+
+        // Append wrong letter to div field of wrong letters
+        var letterInputVal = $letterInput.val();
+        var wrongLetterText = $('.wrongLetter').text();
+        $('.wrongLetter').text(letterInputVal + wrongLetterText);
+      };
+
+      // Clear input field
+      $letterInput.val('');
+
+      // Declare winner or loser
+      var winner = function () {
+        if ($count === $('li').length) {
+          alert('Congrats! You win. Refresh page, and play again!');
+        };
+      };
+
+      // invoke winner/loser functions
+      loser();
+      winner();
+    }; // End If for keypress
+  }); // End keypress event listener
+
+// Event listner for click that does the same as above
   $addLetter.on('click', function() {
 // Filter through split array item to see if input matches any of the letters
     var $count = 0;
